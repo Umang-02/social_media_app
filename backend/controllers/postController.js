513da3ec7bd6e0e5784d43afbd43a0c7b3a68ca2
Umang-02 <umgjhwr@gmail.com>
2024-06-07 23:52:23 +0530

    createPost,getPost,deletePost done

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -7,13 +7,13 @@ const createPost = async(req,res)=>{
         {
             return res.staus(400).json({message:"PostedBy and text fields are required"});
         }
-        const user=User.findById(postedBy);
+        const user= await User.findById(postedBy);
         if(!user)
             return res.status(404).json({message:"User not found"});
-        console.log(user._id);
-        console.log(req.user._id);
+        // console.log(user._id);
+        // console.log(req.user._id.toString());
 
-        if(user._id!==req.user._id)
+        if(user._id.toString()!==req.user._id.toString())
         {
             return res.status(401).json({message:"Unauthorized to create post"});
         }
@@ -32,4 +32,42 @@ const createPost = async(req,res)=>{
     }
 };
 
-export {createPost};
+const getPost = async(req,res)=>{
+    try {
+        const post=await Post.findById(req.params.id);
+        if(!post)
+            return res.status(404).json({message:"Post not found"});
+        
+        res.status(200).json({post});
+
+    } catch (error) {
+        res.status(500).json({message:error.message});
+        console.log("eror in getPost function:",error.message);
+    }
+};
+
+const deletePost=async(req,res)=>{
+    try {
+        const post = await Post.findById(req.params.id);
+        if(!post) 
+            return res.status(404).json({message:"Post not found"});
+        
+        if(post.postedBy.toString()!==req.user._id.toString())
+        {
+            return res.status(401).json({message:"Unauthorized to delete the post"});
+        }
+
+        await Post.findByIdAndDelete(req.params.id);
+        return res.status(200).json({message:"Post deleted successfully"});
+
+    } catch (error) {
+        res.status(500).json({message:error.message});
+        console.log("Error in the deletePost function:",error.message);
+    }
+};
+
+const likeUnlikePost=async(req,res)=>{
+    
+};
+
+export {createPost, getPost , deletePost, likeUnlikePost};
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
 import protectRoute from "../middlewares/protectRoute.js";
-import {createPost} from "../controllers/postController.js";
+import {createPost , getPost, deletePost, likeUnlikePost} from "../controllers/postController.js";
 
 const postRoutes=express.Router();
 
 postRoutes.post("/create",protectRoute,createPost);
+postRoutes.get("/:id",getPost);
+postRoutes.delete("/:id",protectRoute,deletePost);
+postRoutes.post("/like/:id",protectRoute,likeUnlikePost);
 
 export default postRoutes;
